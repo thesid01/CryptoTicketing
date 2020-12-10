941ef651cdf73bfa88d405bdc650cb4ab9ad9b4f
Siddharth <sidkv.skv@gmail.com>
2020-12-10 23:46:11 +0530

    identity management

---
+++ contracts/Approval.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Approval {
+	// using SafeMath for uint;
+    // using SafeMath for uint256;
+    
+    constructor()
+    public{
+        TotalRequests = 0;
+    }
+    
+    struct Request {
+        string aadharNo;
+        string photoProof;
+        bool approved;
+    }
+    
+    mapping(string => bool) aadhar;
+    mapping(address => string) addToAadhar;
+    
+    mapping(uint => Request) requests;
+    uint TotalRequests;
+    mapping(address => bool) approved;
+    mapping(address => bool) requested;
+    
+    event requestCreated(string ssn, string hash);
+    
+    modifier validAadhar(string memory val) {
+        require(aadhar[val] == false);
+        _;
+    }
+    
+    modifier hasRequested(address add) {
+        require(requested[add] == false);
+        _;
+    }
+    
+    modifier validId(uint id) {
+        require(id<TotalRequests);
+        _;
+    }
+    
+    function requestApproval(string memory ssn, string memory hash) 
+    validAadhar(ssn)
+    hasRequested(msg.sender)
+    public
+    {
+        Request memory _request = Request({
+			aadharNo: ssn,
+			photoProof: hash,
+			approved: false
+		});
+		requests[TotalRequests] = _request;
+		TotalRequests+=1;
+		requested[msg.sender] = true;
+		emit requestCreated(ssn, hash);
+    }
+    
+    function approve(uint id)
+    validId(id)
+    public
+    {
+        Request storage _r = requests[id];
+        _r.approved = true;
+        aadhar[_r.aadharNo] = true;
+        addToAadhar[msg.sender] = _r.aadharNo;
+        approved[msg.sender] = true;
+    }
+    
+    function getRequest(uint id)
+    public
+    view
+    returns(string memory, string memory, bool){
+        return (requests[id].aadharNo, requests[id].photoProof, requests[id].approved);
+    }
+    
+    function getTotalRequestsCount()
+    public
+    view
+    returns(uint){
+        return TotalRequests;
+    }
+    
+    function isApproved(address add)
+    public
+    view
+    returns(bool)
+    {
+        return approved[add];
+    }
+}
